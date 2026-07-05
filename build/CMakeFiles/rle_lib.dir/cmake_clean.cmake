fa428b1895d7d54bdd38b7f618038bee428fc492
shya <dan119@bk.ru>
2026-07-05 21:38:25 +0300

    создан CMakeLists.txt

---
+++ build/CMakeFiles/rle_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rle_lib.dir/src/rle_compress_and_decompress.c.o"
+  "CMakeFiles/rle_lib.dir/src/rle_compress_and_decompress.c.o.d"
+  "librle_lib.pdb"
+  "librle_lib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/rle_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
